3b3e259e0e7869141e581c1d8d0452a7ad13334a
SGCMarkus <markusornik@gmail.com>
2019-02-13 08:50:59 +0100

    h930: specify brand, manufacturer and release name
    Change-Id: I8d2a8f4747a4cb3a6b5205349a167e077d1caf7f

--- lineage_h930.mk
+++ lineage_h930.mk
@@ -6,7 +6,12 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Device identifiers
+
 PRODUCT_NAME := lineage_h930
+PRODUCT_BRAND := lge
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_RELEASE_NAME := V30
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="joan" \
