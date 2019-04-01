f86bb67a7040f8724a68cc36302c25507d49a904
SGCMarkus <markusornik@gmail.com>
2019-04-01 18:00:37 +0200

    h930: update fingerprint to h930 21w
    Change-Id: Id7df5d8344aa7762211c8f1236cac421840ba72b

--- lineage_h930.mk
+++ lineage_h930.mk
@@ -13,12 +13,14 @@ PRODUCT_BRAND := lge
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RELEASE_NAME := V30
 
+PRODUCT_GMS_CLIENTID_BASE := android-om-lg
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="joan" \
     PRODUCT_DEVICE="joan" \
     PRODUCT_NAME="joan_global_com" \
-    PRIVATE_BUILD_DESC="joan_global_com-user 8.0.0 OPR1.170623.026 183202318d019 release-keys"
+    PRIVATE_BUILD_DESC="joan_global_com-user 8.0.0 OPR1.170623.026 190211754b47c release-keys"
 
-BUILD_FINGERPRINT="lge/joan_global_com/joan:8.0.0/OPR1.170623.026/183202318d019:user/release-keys"
+BUILD_FINGERPRINT="lge/joan_global_com/joan:8.0.0/OPR1.170623.026/190211754b47c:user/release-keys"
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model ro.vendor.product.model
