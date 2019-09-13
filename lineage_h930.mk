a5bdfe9ae7b3833bc968539bea4e2d0e09ce601d
SGCMarkus <markusornik@gmail.com>
2019-09-13 17:22:29 +0200

    h930: include GSI keys
    Change-Id: I032d13608cc39b5a2b8f2f54805b909d5a5c4773

--- lineage_h930.mk
+++ lineage_h930.mk
@@ -3,6 +3,9 @@ $(call inherit-product, device/lge/h930/full_h930.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
