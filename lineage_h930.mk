64d8ef18bc36d1c832c26a18ed1ef94fee32d135
SGCMarkus <markusornik@gmail.com>
2019-11-07 11:45:16 +0100

    h930: update fingerprint to H930V30q
    Change-Id: Ide2ddfa148e32331dafd26a7ed19c409855bf664

--- lineage_h930.mk
+++ lineage_h930.mk
@@ -25,8 +25,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="joan" \
     PRODUCT_DEVICE="joan" \
     PRODUCT_NAME="joan_global_com" \
-    PRIVATE_BUILD_DESC="joan_global_com-user 9 PKQ1.190414.001 1918215284654 release-keys"
+    PRIVATE_BUILD_DESC="joan_global_com-user 9 PKQ1.190414.001 192451212666b release-keys"
 
-BUILD_FINGERPRINT="lge/joan_global_com/joan:9/PKQ1.190414.001/1918215284654:user/release-keys"
+BUILD_FINGERPRINT="lge/joan_global_com/joan:9/PKQ1.190414.001/192451212666b:user/release-keys"
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model ro.vendor.product.model
