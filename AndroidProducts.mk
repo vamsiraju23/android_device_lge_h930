831df587ab6cbbc3c71d3ed8be0dd9145177eeef
John@023 <43538861+vamsiraju23@users.noreply.github.com>
2020-06-21 12:13:16 +0530

    Stag bring up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,8 +15,8 @@
 #
 
 PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/lineage_h930.mk
+    $(LOCAL_DIR)/stag_h930.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_h930-userdebug \
-    lineage_h930-eng
+    stag_h930-userdebug \
+    stag_h930-eng
